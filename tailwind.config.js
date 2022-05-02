90177eaae08f202a38705dd206fad72bce104a5a
AlexeiMir <alex_16.07@mail.ru>
2022-05-02 11:26:20 +0700

    Add tawlind

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
+
+const primary = '#E30B13';
+
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./app/components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		colors: {
+			primary,
+			black: colors.black,
+			white: colors.white,
+			transparent: colors.transparent,
+			yellow: {
+				700: '#F5C521',
+			},
+			gray: {
+				300: '#d9dae8',
+				500: '#999AA5',
+				600: '#66676E',
+				700: '#39393f',
+				800: '#242529',
+				900: '#191B1F',
+				950: '#101215',
+			},
+		},
+		extend: {
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem',
+			},
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem',
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out',
+			},
+			transitionDuration: {
+				DEFAULT: '200ms',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+		},
+	},
+	plugins: [],
+};
